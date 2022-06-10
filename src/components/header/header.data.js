9571eeb5f04b4bcd55089aaac12c302a5972d7e3
Alp <saeed.partonia@tibber.com>
2022-06-10 23:48:20 +0200

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: "banner",
+    label: "Home",
+  },
+  // {
+  //   path: "services",
+  //   label: "About",
+  // },
+  {
+    path: "news",
+    label: "About",
+  },
+  // {
+  //   path: "news",
+  //   label: "News",
+  // },
+];
