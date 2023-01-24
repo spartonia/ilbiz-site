0085aaaf951d9cca12010be2fbeeb3166aba04b4
soheilqk <soheil.q.k@gmail.com>
2023-01-24 13:59:52 +0330

    adds ts

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Ilbiz Studio</title>
+      <link rel="icon" type="image/png" href="/assets/icon.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
